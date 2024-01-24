fd165b8eb06913c9e8f81354157a0530383f97c7
daryuun5139 <118511364+daryuun5139@users.noreply.github.com>
2024-01-24 09:45:40 +0900

    スマホ対応2

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "@/components/NewComponents/Header";
 import Link from "next/link";
 import Section1 from "@/components/NewComponents/Section1";
@@ -5,12 +7,27 @@ import Section2 from "@/components/NewComponents/Section2";
 import Section3 from "@/components/NewComponents/Section3";
 import Section4 from "@/components/NewComponents/Section4";
 import Section5 from "@/components/NewComponents/Section5";
+import { useEffect } from "react";
 
-export default async function Home() {
+export default function Home() {
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // ここからリサイズの対応
+    // let vw = window.innerWidth;
+    // window.addEventListener("resize", () => {
+    //   if (vw === window.innerWidth) {
+    //     // 画面の横幅にサイズ変動がないので処理を終える
+    //     return;
+    //   }
+    // });
+  });
+
   return (
     <div className="relative w-full">
       <Header />
-      <div className="h-screen w-full snap-y snap-mandatory overflow-hidden overflow-y-scroll">
+      <div className="h-[calc(var(--vh, 1vh) * 100)] h-screen w-full snap-y snap-mandatory overflow-hidden overflow-y-scroll">
         {/* セクション１ Title---------------------------------------------------------------------- */}
         <Section1 />
         {/* セクション２ About---------------------------------------------------------------------- */}
